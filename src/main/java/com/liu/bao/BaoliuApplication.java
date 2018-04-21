15da6723616e2b92547bf7101b949d735407f7fe
aloneliuchao <sxccit@163.com>
2018-04-21 10:09:08 +0800

    Initial commit

---
+++ src/main/java/com/liu/bao/BaoliuApplication.java
@@ -0,0 +1,12 @@
+package com.liu.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoliuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoliuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/liu/bao/BaoliuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liu.bao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaoliuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
